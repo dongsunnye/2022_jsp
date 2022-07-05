738fc3b4a0c7798743d64d0935f4bea23a8a491a
devcation <88315076+devcation-learn@users.noreply.github.com>
2022-07-05 14:16:58 +0900

    회원 관리

--- MemberProject/src/main/java/dao/MemberDAO.java
+++ MemberProject/src/main/java/dao/MemberDAO.java
@@ -1,8 +1,12 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import config.DBManager;
+import dto.MemberDTO;
 
 public class MemberDAO {
 	private static MemberDAO instance = new MemberDAO();
@@ -18,10 +22,38 @@ public class MemberDAO {
 		return instance;
 	}
 
+	public MemberDTO login(String id, String passwd) {
+		MemberDTO dto = null;
+		String sql = "select * from member where id like ? and passwd like ?";
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, id);
+			pstmt.setString(2, passwd);
+			
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				dto = new MemberDTO(rs.getString(1), rs.getString(2), 
+						rs.getString(3), rs.getInt(4), rs.getString(5), rs.getString(6));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			DBManager.getInstance().close(rs,pstmt);
+		}
+		
+		return dto;
+	}
 	
 	
 	
 }
+
+
+
+
 
 
 
