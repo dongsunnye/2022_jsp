3a5caee4a7e5a42527df410626d0337bd6f80515
devcation <88315076+devcation-learn@users.noreply.github.com>
2022-07-08 11:31:14 +0900

    페이징 예제

--- MovieProject/src/main/java/dao/MovieDAO.java
+++ MovieProject/src/main/java/dao/MovieDAO.java
@@ -1,5 +1,11 @@
 package dao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import config.DBManager;
+
 public class MovieDAO {
 	private static MovieDAO instance = new MovieDAO();
 
@@ -11,5 +17,37 @@ public class MovieDAO {
 		return instance;
 	}
 
+	public int selectCount() {
+		int count = 0;
+		String sql = "select count(*) from movie";
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			pstmt = DBManager.getInstance().getConn().prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			if(rs.next())
+				count = rs.getInt(1);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			DBManager.getInstance().close(rs, pstmt);
+		}
+		return count;
+	}
+
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
