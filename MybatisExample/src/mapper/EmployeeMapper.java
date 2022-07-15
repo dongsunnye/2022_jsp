0082f64a5abd7191dd1652a21e1d0f543e644732
devcation <88315076+devcation-learn@users.noreply.github.com>
2022-07-15 15:15:40 +0900

    마이바티스

--- MybatisExample/src/mapper/EmployeeMapper.java
+++ MybatisExample/src/mapper/EmployeeMapper.java
@@ -1,10 +1,15 @@
 package mapper;
 
+import org.apache.ibatis.session.SqlSession;
+
+import config.DBManager;
+
 public class EmployeeMapper {
 	private static EmployeeMapper instance = new EmployeeMapper();
-
+	private SqlSession session;
+	
 	private EmployeeMapper() {
-		
+		session = DBManager.getInstance().getSession();
 	}
 
 	public static EmployeeMapper getInstance() {
