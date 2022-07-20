209190d103b8991d322d5a5b3c4bfda4ced2c2df
devcation <88315076+devcation-learn@users.noreply.github.com>
2022-07-20 11:50:41 +0900

    jUnit

--- MovieMybatisProject/src/main/java/test/MovieTest.java
+++ MovieMybatisProject/src/main/java/test/MovieTest.java
@@ -16,7 +16,7 @@ import config.DBManager;
 import dto.MovieDTO;
 
 class MovieTest {
-
+	static SqlSession session;
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
 		//movie.json 파일 읽어옴
@@ -30,6 +30,8 @@ class MovieTest {
 		}
 		System.out.println(str);
 		JSONArray arr = new JSONArray(str);
+		session = DBManager.getInstance().getTestSession();
+		int count = 0;
 		for(int i=0;i<arr.length();i++) {
 			JSONObject obj = arr.getJSONObject(i);
 			MovieDTO dto = new MovieDTO(obj.getInt("bno"),
@@ -38,13 +40,16 @@ class MovieTest {
 					obj.getString("director"));
 			System.out.println(dto.toString());
 			//읽어온 내용을 movie 테이블에 추가
-			
+			count += session.insert("insertTestMovie", dto);
 		}
+		System.out.println("테스트 데이터 " + count + "건 추가 하였습니다.");
 			
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		session.rollback();
+		System.out.println("테스트 데이터 제거");
 	}
 
 	@Test
