6107214bd5d1c3e3e744070d955ece09d7104aa4
devcation <88315076+devcation-learn@users.noreply.github.com>
2022-07-20 11:33:36 +0900

    jUnit

--- MovieMybatisProject/src/main/java/test/MovieTest.java
+++ MovieMybatisProject/src/main/java/test/MovieTest.java
@@ -5,9 +5,13 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import dto.MovieDTO;
 
 class MovieTest {
 
@@ -23,7 +27,18 @@ class MovieTest {
 			str += t;
 		}
 		System.out.println(str);
-		//읽어온 내용을 movie 테이블에 추가
+		JSONArray arr = new JSONArray(str);
+		for(int i=0;i<arr.length();i++) {
+			JSONObject obj = arr.getJSONObject(i);
+			MovieDTO dto = new MovieDTO(obj.getInt("bno"),
+					obj.getString("title"), 
+					obj.getString("open_date"), obj.getInt("audience_count"),
+					obj.getString("director"));
+			System.out.println(dto.toString());
+			//읽어온 내용을 movie 테이블에 추가
+			
+		}
+			
 	}
 
 	@AfterAll
