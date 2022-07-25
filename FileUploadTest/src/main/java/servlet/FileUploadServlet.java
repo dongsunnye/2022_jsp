9555bf2bef054e18a73cdcb53be4855121ae30f3
devcation <88315076+devcation-learn@users.noreply.github.com>
2022-07-25 10:31:43 +0900

    파일 업로드

--- FileUploadTest/src/main/java/servlet/FileUploadServlet.java
+++ FileUploadTest/src/main/java/servlet/FileUploadServlet.java
@@ -37,7 +37,8 @@ public class FileUploadServlet extends HttpServlet {
 		request.setCharacterEncoding("utf-8");
 		String encoding = "utf-8";
 		
-		File userRoot = new File("");
+		File userRoot = new File(request.getSession().getServletContext().getRealPath("/")
+				+"/upload");
 		
 		DiskFileItemFactory factory = new DiskFileItemFactory();
 		factory.setRepository(userRoot);//업로드될 폴더 설정
@@ -47,11 +48,35 @@ public class FileUploadServlet extends HttpServlet {
 		
 		try {
 			List<FileItem> list = upload.parseRequest(request);
+			for(FileItem item : list) {
+				if(item.isFormField()) {
+					System.out.println(item.getFieldName() + "=" + item.getString(encoding));
+				}else {
+					System.out.println("매개변수명 : " + item.getFieldName());
+					System.out.println("파일명 : " + item.getName());
+					System.out.println("파일크기 : " + item.getSize());
+					System.out.println("파일타입 : " + item.getContentType());
+					if(item.getSize() > 0) {
+						int idx = item.getName().lastIndexOf("\\");
+						if(idx == -1)
+							idx = item.getName().lastIndexOf("/");
+						
+						String fileName = item.getName().substring(idx + 1);
+						File uploadFile = new File(userRoot + "\\" + fileName);
+						item.write(uploadFile);
+						request.setAttribute("file1", request.getContextPath()
+								+ "/upload/"+fileName);
+					}
+				}
+				
+			}
 		} catch (FileUploadException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	
-	
+		request.getRequestDispatcher("file_result.jsp").forward(request, response);
 	}
 
 	/**
